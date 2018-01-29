999f3ca2a2f2ae9e10ab271e296ded0ff34cb74d
Xuming Zeng <xsznix@gmail.com>
2018-01-29 14:31:10 -0600

    v2.1.1: Don't show changelog on the next run after onboarding for a new install

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
   if (await Onboarding.shouldRun()) {
     await Onboarding.show();
     await Onboarding.markAsComplete();
+    await Changelog.markAsComplete();
   } else if (await Changelog.shouldRun()) {
     await Changelog.show();
     await Changelog.markAsComplete();
